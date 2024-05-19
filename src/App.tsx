c72d9fbdee1e633d61bd18d21671c3017211e3eb
Toasterf1sh <106944368+Toasterf1sh@users.noreply.github.com>
2024-05-20 03:01:54 +1000

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import bubbles from "./bubbles.png";
+import bubbles2 from "./bubbles2.png";
+import bubbles3 from "./bubbles3.png";
+import bubbles4 from "./bubbles4.png";
+import clouds from "./clouds.png";
+import cloud2 from "./cloud2.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState(<span>click bubbles</span>);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      <p className="initial-text">{text}</p>
+      <div className="hobbies-bubble">
+        <button
+          className="bubble"
+          onClick={() => {
+            setText(
+              <span>
+                <div className="hobbies-container">
+                  <h1 className="about-me-title">My hobbies</h1>
+                  <p>
+                    I've been a casual maimai player for about a year now (I
+                    still suck though).
+                  </p>
+                  <p>
+                    I also cycle through mobile/idle games because it keeps me
+                    distracted on the long trek to UNSW. Notable games include:
+                    Candy Crush, HookedInc, Camping Tycoon, Good Pizza Great
+                    Pizza. Currently I am on level __ of wordscapes (i'll
+                    probably get bored soon)
+                  </p>
+                  <p>
+                    Recently, I've picked up crocheting. I can make flowers.
+                  </p>
+                  <p>
+                    Also sometimes I bake things.
+                  </p>
+                </div>
+              </span>
+            );
+          }}
+        >
+          <img src={bubbles2} className="bubble-image"></img>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className="about-me-bubble">
+        <button
+          className="bubble"
+          onClick={() => {
+            setText(
+              <span>
+                <div className="about-me-container">
+                  <h1 className="about-me-title">About me</h1>
+                  <p>
+                    Hi, I'm Alyssa - a first year compsci/engineering student.
+                    When i'm not studying, i'm probably playing video games or
+                    trying (and failing) to code random stuff (chronically
+                    online).
+                  </p>
+                </div>
+              </span>
+            );
+          }}
+        >
+          <img src={bubbles} className="bubble-image"></img>
+        </button>
+      </div>
+
+      <div className="code-bubble">
+        <button
+          className="bubble"
+          onClick={() => {
+            setText(
+              <span>
+                <div className="code-container">
+                <h1 className="about-me-title">Some Irrelevant Projects</h1>
+                <p>I made this pizza game in p5js.</p>
+                <p>Also this chicken</p>
+                </div>
+              </span>
+            )
+          }}
+        
+        
+        
+        ><img src={bubbles3} className="bubble-image"></img></button>
+      </div>
+
+
+
+    </div>
+  );
 }
 
-export default App
+export default App;
